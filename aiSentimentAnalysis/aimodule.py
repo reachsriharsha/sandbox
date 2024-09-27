c27fb5e5d8feeabd04d3187ec1132f3359bf1e58
sharsha <sharsha@rbbn.com>
2024-09-27 06:50:33 +0000

    sentiment analysis

--- aiSentimentAnalysis/aimodule.py
+++ aiSentimentAnalysis/aimodule.py
@@ -1,5 +1,7 @@
 from langchain_community.llms import Ollama
 from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_ollama import ChatOllama
+
 #from langchain_experimental.llms.ollama_functions import OllamaFunctions
 
 
@@ -9,6 +11,11 @@ class SentimentResult(BaseModel):
     """Sentiment Result"""
     sentiment: str = Field(..., description="Sentiment of the text")
     score: float = Field(..., description="Sentiment score of the text")
+    summary: str = Field(..., description="Summary of the text")
+    input_tokens: int = Field(..., description="Number of tokens in the input text")
+    output_tokens: int = Field(..., description="Number of tokens in the output text")
+    total_tokens: int = Field(..., description="Number of total tokens")
+
 
 
 
@@ -20,17 +27,22 @@ class SentimentAnalysis:
 
     def predict_sentiment(self, text):
 
-        llm = Ollama(model="llama3")
+        llm = ChatOllama(model="llama3.1:latest",temperature=0,verbose=True)
+        #llm = ChatOllama(model="tinyllama:latest",temperature=0,verbose=True)
         #llm = OllamaFunctions(model="llama3")
         messages = [
-            ("system", "You are a helpful assistant that summerizes the given context"),
+            ("system", "You are a helpful assistant that summerizes the given text"),
             ("human", text)
         ]
 
-        #structured_response = llm.with_strucured_output(SentimentResult)
+        structured_response = llm.with_structured_output(SentimentResult)
 
-        prediction = llm.invoke(messages)
+        #prediction = llm.invoke(messages)
+        prediction = structured_response.invoke(messages)
 
-        print(f"AI Response: {prediction}") 
+        print(type(prediction))
+
+
+        print(prediction) 
         return prediction
         
